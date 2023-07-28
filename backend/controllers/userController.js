117352c553a55c49fc0c40b8a3a18f37b21cd324
ingstadshakthi <59392423+ingstadshakthi@users.noreply.github.com>
2023-07-29 01:46:22 +0530

    added functionality
    1. added signin
    2. added signup
    3. added logout

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,39 +1,71 @@
 const asyncHandler = require('express-async-handler');
-const User = require("./../models/userModel");
-
+const User = require('./../models/userModel');
+const generateToken = require('./../utils/generateToken');
 // @desc Auth user/set token
 // route POST /api/users/auth
 // @access Public
 exports.authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
 
-  res.status(200).json({ message: 'Auth User' });
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPasswords(password))) {
+    generateToken(res, user._id);
+    res.status(201).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid Email or password');
+  }
 });
-
 
 // @desc Register a new User
 // route POST /api/users
 // @access Public
 exports.registerUser = asyncHandler(async (req, res) => {
-
   const { name, email, password } = req.body;
-  console.log(name, email, password)
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error('User already exists');
+  }
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
 
-  res.status(200).json({ message: 'Auth User' });
+  if (user) {
+    generateToken(res, user._id);
+    res.status(201).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid User Data');
+  }
 });
 
 // @desc Logout User
 // route POST /api/users/logout
 // @access Public
 exports.logoutUser = asyncHandler(async (req, res) => {
-
-  res.status(200).json({ message: 'Auth User' });
+  res.cookie('jwt', '', {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+  res.status(200).json({ message: 'User logged out' });
 });
 
 // @desc Get user profile
 // route GET /api/users/profile
 // @access private
 exports.getUserProfile = asyncHandler(async (req, res) => {
-
   res.status(200).json({ message: 'Auth User' });
 });
 
@@ -41,6 +73,5 @@ exports.getUserProfile = asyncHandler(async (req, res) => {
 // route PUT /api/users/profile
 // @access private
 exports.updateUserProfile = asyncHandler(async (req, res) => {
-
   res.status(200).json({ message: 'Update User Profile' });
 });
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,22 +1,38 @@
-const mongoose = require("mongoose");
-const userSchema = mongoose.Schema({
-  name: {
-    required: true,
-    type: String
-  },
-  email: {
-    required: true,
-    type: String,
-    unique: true
-  },
-  password: {
-    required: true,
-    type: String
-  }
-}, { timestamps: true });
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      required: true,
+      type: String,
+    },
+    email: {
+      required: true,
+      type: String,
+      unique: true,
+    },
+    password: {
+      required: true,
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+userSchema.methods.matchPasswords = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
 const User = mongoose.model('User', userSchema);
-
 
 module.exports = User;
