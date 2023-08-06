8082357a72f9c750f1ca090df486609dc8fbae04
ingstadshakthi <59392423+ingstadshakthi@users.noreply.github.com>
2023-08-06 23:27:10 +0530

    environment
    environment

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -4,7 +4,6 @@
 // //   next(error);
 // // }
 
-
 // exports.errorHandler = (err, req, res, next) => {
 //   let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
 //   let message = err.message;
@@ -25,61 +24,67 @@ const AppError = require('./../utils/appError');
 const handleCastErrorDB = (err) => {
   const message = `Invalid ${err.path}: ${err.value}`;
   return new AppError(message, 400);
-}
+};
 
 const handleDuplicateIndexDB = (err) => {
   const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
   const message = `Duplicate Value ${value}`;
   return new AppError(message, 400);
-}
+};
 
-const handleValidationErrorDB = err => {
-  const errors = Object.values(err.errors).map(el => el.message);
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
   const message = `Invalid input data ${errors.join('. ')}`;
   return new AppError(message, 400);
-}
+};
 
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
     error: err,
     message: err.message,
-    stack: err.stack
+    stack: err.stack,
   });
-}
+};
 
 const sendErrorProd = (err, res) => {
-  if (err.isOperational) { // trusted error : can be sent to client
+  if (err.isOperational) {
+    // trusted error : can be sent to client
     res.status(err.statusCode).json({
       status: err.status,
-      message: err.message
+      message: err.message,
     });
-  } else { // unknown error we do not want to leak the details
+  } else {
+    // unknown error we do not want to leak the details
 
     // 1) Log the error
-    console.error("ERROR ", err);
+    console.error('ERROR ', err);
 
     // 2) Send generic message
     res.status(500).json({
       status: 'error',
-      message: 'Something went wrong'
+      message: 'Something went wrong',
     });
   }
-}
+};
 
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  if (process.env.NODE_ENV === "development") {
+  if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
-  } else if (process.env, NODE_ENV === "production") {
+  } else if ((process.env, NODE_ENV || 'production' === 'production')) {
     let error = { ...err };
-    if (err.name === "CastError") { error = handleCastErrorDB(error) }
-    if (err.code === 11000) { error = handleDuplicateIndexDB(error) }
-    if (err.name === "ValidationError") { error = handleValidationErrorDB(error) }
+    if (err.name === 'CastError') {
+      error = handleCastErrorDB(error);
+    }
+    if (err.code === 11000) {
+      error = handleDuplicateIndexDB(error);
+    }
+    if (err.name === 'ValidationError') {
+      error = handleValidationErrorDB(error);
+    }
     sendErrorProd(err, res);
   }
-
-
-}
+};
